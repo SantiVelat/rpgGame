784e966ba8e768f3c1da61e495fb3645c0de845f
santi <snt.mv13@gmail.com>
2017-08-28 23:17:37 +0200

    jade implemented

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, '../client')))
+
+app.get('/', (req, res)=>{
+	res.render('home')
+})
+
+app.get('/about', (req, res)=>{
+	res.render('about')
+})
+
+app.get('/score', (req, res)=>{
+    res.render('global-score')
+})
+
+app.get('/register', (req, res)=>{
+	res.render('register')
+})
+
+app.listen(process.env.PORT || 3002)
+
