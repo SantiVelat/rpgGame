b4140fce62cb9d568141c98d54baad50a3e88f35
santi <snt.v.01@gmail.com>
2017-08-29 09:54:38 +0200

    heroku app fixed

--- server/app.js
+++ server/app.js
@@ -2,25 +2,23 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, '../client')))
-
-app.get('/', (req, res)=>{
-	res.render('home')
+app.set('views', path.join(__dirname, 'views'))
+app.get('/', (req, res) => {
+  res.render('home')
 })
 
-app.get('/about', (req, res)=>{
-	res.render('about')
+app.get('/about', (req, res) => {
+  res.render('about')
 })
 
-app.get('/score', (req, res)=>{
-    res.render('global-score')
+app.get('/score', (req, res) => {
+  res.render('global-score')
 })
 
-app.get('/register', (req, res)=>{
-	res.render('register')
+app.get('/register', (req, res) => {
+  res.render('register')
 })
 
 app.listen(process.env.PORT || 3002)
-
