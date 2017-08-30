33aa3f776607858e86e235edd54d9b448222b281
santi <snt.mv13@gmail.com>
2017-08-30 09:04:30 +0200

    Controllers implemented

---
+++ client/app/controllers/CharacterController.js
@@ -0,0 +1,9 @@
+/* global game */
+
+game.module('rpGame')
+  .controller('CharacterController', function () {
+  		const self = this
+  		let currentHealth = 100
+
+  		let healthPercentage = (100/300)*100
+  })
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -3,5 +3,13 @@
   const game = angular.module('rpGame')
   game.service('DataService', function ($https) {
     const self = this
-  })
-})
+    self.getGamePath=function(roadUrl, callback){
+		    	 $http.get(roadUrl)
+                    .then(function(response) {
+                    	console.log(response.data);
+                    	callback(response.data)
+                	})
+		}
+	});
+ })
+
