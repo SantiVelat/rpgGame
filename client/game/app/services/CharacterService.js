1fb5984bb76a79efddc3f641f09cc653508cf819
santi <snt.v.01@gmail.com>
2017-09-04 14:50:02 +0200

    Character Health fixed

--- client/game/app/services/CharacterService.js
+++ client/game/app/services/CharacterService.js
@@ -1,19 +1,19 @@
 /* global angular */
-(function() {
-    angular.module('rpGame')
-        .service('CharacterService', function() {
-            let currentHealth = 80
+(function () {
+  angular.module('rpGame')
+        .service('CharacterService', function () {
+          let currentHealth = 80
 
-            this.setCurrentHealth = (health) => {
-                currentHealth = Math.round(health)
+          this.setCurrentHealth = (health) => {
+            currentHealth = health > 0 ? Math.round(health) : 0
 
-                if (currentHealth <= 0) {
-                    $location.path('gameover')
-                }
+            if (currentHealth === 0) {
+              $location.path('gameover')
             }
+          }
 
-            this.getCurrentHealth = () => {
-                return currentHealth
-            }
+          this.getCurrentHealth = () => {
+            return currentHealth
+          }
         })
 })()
